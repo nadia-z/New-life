7f5dffb09cfbf2397a03cc89613507b9d5924877
eitan hoyte <eitanhoyte12@gmail.com>
2025-06-27 19:44:28 +0100

    built show edit and destroy CRUD actions in lives controller, added form for edit.html.erb, added some html for show.html.erb

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -3,7 +3,7 @@ class LivesController < ApplicationController
   end
 
   def show
-    #@life = Life.find(params[:id])
+    @life = Life.find(params[:id])
   end
 
   def new
@@ -17,15 +17,27 @@ class LivesController < ApplicationController
   end
 
   def update
+    life = Life.find(params[:id])
+    life.title = params[:title]
+    life.description = params[:description]
+    life.address = params[:address]
+    life.price_per_day = params[:price_per_day]
+    life.status = params[:status]
+    life.save
 
+
+    redirect_to life_path(params[:id])
   end
 
   def destroy
-    @life = Life.find(params[:id])
+    life = Life.find(params[:id])
     life.destroy
+
+    redirect_to lives_path
   end
 
   private
+
 
 
 end
