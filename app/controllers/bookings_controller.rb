281b773299f45cdb7c0a69fd8ee5d47a9ce69162
CASSIUS_AZV <75045215+nicolabergamaschi@users.noreply.github.com>
2025-06-30 22:20:39 +0100

    Update app/controllers/bookings_controller.rb
    Co-authored-by: nadia-z <33801529+nadia-z@users.noreply.github.com>

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -43,7 +43,13 @@ class BookingsController < ApplicationController
   end
 
   private
+def set_life
+    @life = Life.find(params[:life_id])
+  end
 
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
