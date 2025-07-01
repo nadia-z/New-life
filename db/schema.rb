59f7bc5062b57f8c4dca22e2a3902c04c3f9d895
Nadia Zeissig <33801529+nadia-z@users.noreply.github.com>
2025-07-01 11:09:25 +0200

    life-card and card container styling,colors,added preliminary search bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_26_213533) do
-
+ActiveRecord::Schema[7.1].define(version: 2025_06_26_072415) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
+    t.bigint "life_id", null: false
+    t.bigint "user_id", null: false
     t.date "start_date"
     t.date "end_date"
-    t.bigint "user_id", null: false
-    t.bigint "life_id", null: false
+    t.float "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "total_price"
     t.index ["life_id"], name: "index_bookings_on_life_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "lives", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
     t.string "title"
     t.string "description"
     t.string "address"
     t.float "price_per_day"
+    t.string "category"
     t.string "status"
     t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_lives_on_user_id"
   end
 
