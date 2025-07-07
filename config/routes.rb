6c084be05350faa546f51a44f611877e16e6a4f1
nadia-z <33801529+nadia-z@users.noreply.github.com>
2025-07-07 17:26:47 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
-  get 'messages/new'
-  get 'messages/create'
   devise_for :users
   root to: "pages#home"
+  get '/about', to: 'pages#about'
 
   resources :lives do
-    resources :reviews, only: [:new, :create]
+    resources :bookings
+      resources :reviews, only: [:new, :create]
   end
 
+  get "/users/show", to: "users#show"
+  patch "/users/show", to: "users#update"
+
+  get '/bookings', to: 'bookings#index', as: 'bookings'
+  get '/bookings/:id', to: 'bookings#show', as: 'booking'
+
+
   get "up" => "rails/health#show", as: :rails_health_check
+
 end
